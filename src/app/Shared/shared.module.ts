03ab6c69b5b528981d618fd803b0877c71d55e46
marlacp <macapripe@gmail.com>
2019-11-01 17:40:02 -0500

    Creacion de Shared.module

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ]
