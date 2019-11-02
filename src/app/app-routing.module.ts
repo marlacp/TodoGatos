5b6f468ee22c0e1fb6d858d918df2834ccc4db53
marlacp <macapripe@gmail.com>
2019-11-01 22:25:41 -0500

    enrutamiento del layout clave para el funcionamiento de la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  // aqui es importante en rutar el layout
+  component: LayoutComponent,
+}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // all modules are preloaded as fast as possible
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
