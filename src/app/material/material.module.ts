17bb1422c647788dcdddb381047b7a6285a3c577
marlacp <macapripe@gmail.com>
2019-11-05 14:00:24 -0500

    se implementa las librerias de material.module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,10 +3,68 @@ import { CommonModule } from '@angular/common';
 
 
 
+import { MatBadgeModule } from '@angular/material/badge';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSortModule } from '@angular/material/sort';
+import { LayoutModule } from '@angular/cdk/layout';
+import {MatStepperModule} from '@angular/material/stepper';
+
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatMenuModule,
+    LayoutModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatStepperModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatMenuModule,
+    LayoutModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatStepperModule
   ]
 })
 export class MaterialModule { }
