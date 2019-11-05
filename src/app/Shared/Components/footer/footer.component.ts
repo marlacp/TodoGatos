247c3064f99ff3c42abd37da1d2365bbfeee3879
marlacp <macapripe@gmail.com>
2019-11-05 17:06:34 -0500

    se crea las validaciones y la funcion de SendEmail

--- src/app/Shared/Components/footer/footer.component.ts
+++ src/app/Shared/Components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  emailField: FormControl;
+  constructor() {
+    this.emailField = new FormControl('', [
+      Validators.required,
+      Validators.email]);
+  }
 
   ngOnInit() {
   }
 
+  // si ejecuto el metodo emailField  y si es valido me devuelve el valor
+  sendMail() {
+    if (this.emailField.valid) {
+      console.log(this.emailField.value);
+    }
+  }
 }
