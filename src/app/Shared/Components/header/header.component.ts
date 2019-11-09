ba4552b0ae449a95d51fdbe37554b4bd493ade43
marlacp <macapripe@gmail.com>
2019-11-09 11:52:11 -0500

    se crea el servicio cart y se llama desde el header para realizar el contador del carrito

--- src/app/Shared/Components/header/header.component.ts
+++ src/app/Shared/Components/header/header.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from './../../../Core/Services/cart.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  total = 0;
+  constructor(
+    private cartService: CartService
+  ) {
+    // 1ra manera de hacerlo
+    // escuchamos dinamicamente nuestro carrito
+    this.cartService.cart$.subscribe( products => {
+      console.log(products);
+      this.total = products.length;
+    });
+  }
 
   ngOnInit() {
   }
