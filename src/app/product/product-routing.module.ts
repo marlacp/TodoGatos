4e5dd6ed5deb9cbaff140b94cf801e2a05867d8d
marlacp <macapripe@gmail.com>
2019-11-12 16:17:56 -0500

    se agrega el path detail con la ruta id

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductsComponent } from './Components/products/products.component';
+import { ProductDetailComponent } from './Components/product-detail/product-detail.component';
 
 const routes: Routes = [{
   path: '',
   component: ProductsComponent
-}];
+  },
+  {
+    path: ':id',
+    component: ProductDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
