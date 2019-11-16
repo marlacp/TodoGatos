b481ac8d6e01d70ae5a0bdc3cf89a11b4fb55d63
marlacp <macapripe@gmail.com>
2019-11-15 23:01:27 -0500

    se realiza el home, con un slide animado

--- src/app/Home/home.module.ts
+++ src/app/Home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './Components/home/home.component';
+import { BannerComponent } from './Components/banner/banner.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, BannerComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
